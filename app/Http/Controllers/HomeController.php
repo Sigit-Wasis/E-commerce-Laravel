a4682fd86c55b61a04c817eb7f306ea36f2ae13d
sigit wasis subekti <sigitghoticmetal2001@gmail.com>
2020-06-19 22:45:06 +0700

    Laporan Order Dengan Date Range

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Order;
+use PDF;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
@@ -24,5 +27,42 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function orderReport()
+    {
+        //INISIASI 30 HARI RANGE SAAT INI JIKA HALAMAN PERTAMA KALI DI-LOAD
+        //KITA GUNAKAN STARTOFMONTH UNTUK MENGAMBIL TANGGAL 1
+        $start = Carbon::now()->startOfMonth()->format('Y-m-d H:i:s');
+        //DAN ENDOFMONTH UNTUK MENGAMBIL TANGGAL TERAKHIR DIBULAN YANG BERLAKU SAAT INI
+        $end = Carbon::now()->endOfMonth()->format('Y-m-d H:i:s');
+
+        //JIKA USER MELAKUKAN FILTER MANUAL, MAKA PARAMETER DATE AKAN TERISI
+        if (request()->date != '') {
+            //MAKA FORMATTING TANGGALNYA BERDASARKAN FILTER USER
+            $date = explode(' - ' ,request()->date);
+            $start = Carbon::parse($date[0])->format('Y-m-d') . ' 00:00:01';
+            $end = Carbon::parse($date[1])->format('Y-m-d') . ' 23:59:59';
+        }
+
+        //BUAT QUERY KE DB MENGGUNAKAN WHEREBETWEEN DARI TANGGAL FILTER
+        $orders = Order::with(['customer.district'])->whereBetween('created_at', [$start, $end])->get();
+        //KEMUDIAN LOAD VIEW
+        return view('report.order', compact('orders'));
+    }
+
+    public function orderReportPdf($daterange)
+    {
+        $date = explode('+', $daterange); //EXPLODE TANGGALNYA UNTUK MEMISAHKAN START & END
+        //DEFINISIKAN VARIABLENYA DENGAN FORMAT TIMESTAMPS
+        $start = Carbon::parse($date[0])->format('Y-m-d') . ' 00:00:01';
+        $end = Carbon::parse($date[1])->format('Y-m-d') . ' 23:59:59';
+
+        //KEMUDIAN BUAT QUERY BERDASARKAN RANGE CREATED_AT YANG TELAH DITETAPKAN RANGENYA DARI $START KE $END
+        $orders = Order::with(['customer.district'])->whereBetween('created_at', [$start, $end])->get();
+        //LOAD VIEW UNTUK PDFNYA DENGAN MENGIRIMKAN DATA DARI HASIL QUERY
+        $pdf = PDF::loadView('report.order_pdf', compact('orders', 'date'));
+        //GENERATE PDF-NYA
+        return $pdf->stream();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -84,5 +84,11 @@ Route::group(['prefix' => 'administrator', 'middleware' => 'auth'], function() {
         Route::get('/return/{invoice}', 'OrderController@return')->name('orders.return');
         Route::post('/return', 'OrderController@approveReturn')->name('orders.approve_return');
     });
+
+    Route::group(['prefix' => 'reports'], function() {
+        Route::get('/order', 'HomeController@orderReport')->name('report.order');
+        Route::get('/order/pdf/{daterange}', 'HomeController@orderReportPdf')->name('report.order_pdf');
+      
+    });
 });
 
